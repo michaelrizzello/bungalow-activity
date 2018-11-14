748d004ee338dae9251424b330c3a322d725b9d5
Rizzello, Michael <MRizzello@cara.com>
2018-11-14 12:47:50 -0500

    removed postgres

--- bungalow/bungalow/settings.py
+++ bungalow/bungalow/settings.py
@@ -76,20 +76,10 @@ WSGI_APPLICATION = 'bungalow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bun',
-        'USER': 'mrizzello',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
